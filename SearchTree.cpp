56f347783c3566e59511babf1d7fd5480bac2c2a
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-06-11 16:02:58 +0700

    menambahkan pointer parent, currentNode, dan memanggil fungsi search

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -28,12 +28,13 @@ public:
 
     void insert(string element)
     {
-         Node *newNode = new Node(element, NULL, NULL);
+        Node *newNode = new Node(element, NULL, NULL);
 
         newNode->info = element;
         newNode->leftchild = NULL;
         newNode->rightchild = NULL;
 
-        
-    }
-};
+        Node *parent = NULL;
+        Node *currentNode = NULL;
+
+        search(element, parent, currentNode);
