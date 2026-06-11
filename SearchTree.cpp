72720875c2ae68d5a41c528589f7007cbbe9a299
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-06-11 16:03:49 +0700

    menangani kondisi jika tree masih kosong di fungsi insert

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -38,3 +38,10 @@ public:
         Node *currentNode = NULL;
 
         search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
+
